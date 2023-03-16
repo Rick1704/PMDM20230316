7174599f145b723e73f7e485935bc06007a362e8
Rick1704 <ricardosf00@educastur.es>
2023-03-16 18:19:35 +0100

    commit

---
+++ app/src/main/java/fp/dam/pmdm/examen20230316/Fragmento2.java
@@ -0,0 +1,20 @@
+package fp.dam.pmdm.examen20230316;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragmento2 extends Fragment {
+    public Fragmento2() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragmento2, container, false);
+    }
+}
