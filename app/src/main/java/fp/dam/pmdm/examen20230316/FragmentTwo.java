b2eccbcae65ebb02818a5bfdd0e5d4bc1b32da42
ricardosf00 <Vreser302sofrer8274jrus>
2023-03-22 03:16:30 +0100

    segundoFragment

--- app/src/main/java/fp/dam/pmdm/examen20230316/FragmentTwo.java
+++ app/src/main/java/fp/dam/pmdm/examen20230316/FragmentTwo.java
@@ -1,6 +1,20 @@
 package fp.dam.pmdm.examen20230316;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class FragmentTwo extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragmenttwo, container, false);
+        return view;
+    }
+
 }
